27454a3445e1a74573a448c2062ee3165b17d191
Sap <sap@polidius.dev>
2023-01-09 23:16:43 +0100

    Update head.tsx

--- app/head.tsx
+++ app/head.tsx
@@ -1,9 +1,9 @@
 export default function Head() {
   return (
     <>
-      <title>The Counter</title>
+      <title>The TOTK Counter</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <meta name="description" content="The Legends of zelda Tears of The Kingdom Live Counter." />
+      <meta name="description" content="The Legends of Zelda TOTK's Live Counter. Made by Sir Sap <3" />
       <link rel="icon" href="/favicon.ico" />
     </>
   )
