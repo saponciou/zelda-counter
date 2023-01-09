2df0f94eec06a5467727ca751a4a5991c40effc4
Sap <sap@polidius.dev>
2023-01-09 23:25:39 +0100

    Update head.tsx

--- app/head.tsx
+++ app/head.tsx
@@ -4,6 +4,7 @@ export default function Head() {
       <title>The TOTK Counter</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="The Legends of Zelda TOTK's Live Counter. Made by Sir Sap <3" />
+      <meta itemprop="image" content="https://images-ext-1.discordapp.net/external/UsproHAbfOIcVn_OkfBrERlbU5Ols5NBRxwfHwOt-rw/https/cdn.wikimg.net/en/zeldawiki/images/7/7e/BotW_Hyrule_Sunset_Artwork.jpg">
       <link rel="icon" href="/favicon.ico" />
     </>
   )
