3a3976677e6fd2ac3488400312d71f45f082ccd0
Sap <sap@polidius.dev>
2023-01-09 23:28:31 +0100

    Update head.tsx

--- app/head.tsx
+++ app/head.tsx
@@ -2,23 +2,16 @@ export default function Head() {
   return (
     <>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <!-- HTML Meta Tags -->
       <title>The TOTK Counter</title>
       <meta name="description" content="The Legends of Zelda TOTK's Live Counter. Made by Sir Sap <3">
-
-      <!-- Google / Search Engine Tags -->
       <meta itemprop="name" content="The TOTK Counter">
       <meta itemprop="description" content="The Legends of Zelda TOTK's Live Counter. Made by Sir Sap <3">
       <meta itemprop="image" content="https://images-ext-1.discordapp.net/external/UsproHAbfOIcVn_OkfBrERlbU5Ols5NBRxwfHwOt-rw/https/cdn.wikimg.net/en/zeldawiki/images/7/7e/BotW_Hyrule_Sunset_Artwork.jpg">
-
-      <!-- Facebook Meta Tags -->
       <meta property="og:url" content="https://zelda-counter.vercel.app">
       <meta property="og:type" content="website">
       <meta property="og:title" content="The TOTK Counter">
       <meta property="og:description" content="The Legends of Zelda TOTK's Live Counter. Made by Sir Sap <3">
       <meta property="og:image" content="https://images-ext-1.discordapp.net/external/UsproHAbfOIcVn_OkfBrERlbU5Ols5NBRxwfHwOt-rw/https/cdn.wikimg.net/en/zeldawiki/images/7/7e/BotW_Hyrule_Sunset_Artwork.jpg">
-
-      <!-- Twitter Meta Tags -->
       <meta name="twitter:card" content="summary_large_image">
       <meta name="twitter:title" content="The TOTK Counter">
       <meta name="twitter:description" content="The Legends of Zelda TOTK's Live Counter. Made by Sir Sap <3">
