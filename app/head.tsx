baa38654d83c6a2bf70131bf2120d5545935b441
Sap <sap@polidius.dev>
2023-01-10 15:44:11 +0100

    Update head.tsx

--- app/head.tsx
+++ app/head.tsx
@@ -3,19 +3,20 @@ export default function Head() {
     <>
       <title>The TOTK Counter</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <meta name="description" content="The Legends of Zelda TOTK's Live Counter. Made by Sir Sap <3">
-      <meta itemprop="name" content="The TOTK Counter">
-      <meta itemprop="description" content="The Legends of Zelda TOTK's Live Counter. Made by Sir Sap <3">
-      <meta itemprop="image" content="https://images-ext-1.discordapp.net/external/UsproHAbfOIcVn_OkfBrERlbU5Ols5NBRxwfHwOt-rw/https/cdn.wikimg.net/en/zeldawiki/images/7/7e/BotW_Hyrule_Sunset_Artwork.jpg">
-      <meta property="og:url" content="https://zelda-counter.vercel.app">
-      <meta property="og:type" content="website">
-      <meta property="og:title" content="The TOTK Counter">
-      <meta property="og:description" content="The Legends of Zelda TOTK's Live Counter. Made by Sir Sap <3">
-      <meta property="og:image" content="https://images-ext-1.discordapp.net/external/UsproHAbfOIcVn_OkfBrERlbU5Ols5NBRxwfHwOt-rw/https/cdn.wikimg.net/en/zeldawiki/images/7/7e/BotW_Hyrule_Sunset_Artwork.jpg">
-      <meta name="twitter:card" content="summary_large_image">
-      <meta name="twitter:title" content="The TOTK Counter">
-      <meta name="twitter:description" content="The Legends of Zelda TOTK's Live Counter. Made by Sir Sap <3">
-      <meta name="twitter:image" content="https://images-ext-1.discordapp.net/external/UsproHAbfOIcVn_OkfBrERlbU5Ols5NBRxwfHwOt-rw/https/cdn.wikimg.net/en/zeldawiki/images/7/7e/BotW_Hyrule_Sunset_Artwork.jpg">
+      <meta name="description" content="The Legends of Zelda TOTK's Live Counter. Made by Sir Sap <3" />
+      <meta itemprop="name" content="The TOTK Counter" />
+      <meta itemprop="description" content="The Legends of Zelda TOTK's Live Counter. Made by Sir Sap <3" />
+      <meta itemprop="image" content="https://images-ext-1.discordapp.net/external/UsproHAbfOIcVn_OkfBrERlbU5Ols5NBRxwfHwOt-rw/https/cdn.wikimg.net/en/zeldawiki/images/7/7e/BotW_Hyrule_Sunset_Artwork.jpg" />
+      <meta property="og:url" content="https://zelda-counter.vercel.app" />
+      <meta property="og:type" content="website"/>
+      <meta property="og:title" content="The TOTK Counter" />
+      <meta property="og:description" content="The Legends of Zelda TOTK's Live Counter. Made by Sir Sap <3" />
+
+      <meta property="og:image" content="https://images-ext-1.discordapp.net/external/UsproHAbfOIcVn_OkfBrERlbU5Ols5NBRxwfHwOt-rw/https/cdn.wikimg.net/en/zeldawiki/images/7/7e/BotW_Hyrule_Sunset_Artwork.jpg" />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content="The TOTK Counter" />
+      <meta name="twitter:description" content="The Legends of Zelda TOTK's Live Counter. Made by Sir Sap <3" />
+      <meta name="twitter:image" content="https://images-ext-1.discordapp.net/external/UsproHAbfOIcVn_OkfBrERlbU5Ols5NBRxwfHwOt-rw/https/cdn.wikimg.net/en/zeldawiki/images/7/7e/BotW_Hyrule_Sunset_Artwork.jpg" />
       
       <link rel="icon" href="/favicon.ico" />
     </>
